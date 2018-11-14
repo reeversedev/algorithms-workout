08925fac1043aa869bd154495e36b0b7a06698a8
Prateek Gogia <prateek_gogia@gridle.io>
2018-11-14 10:16:26 +0530

    Added comments

--- a-rule-of-divisibility-by-13/index.js
+++ a-rule-of-divisibility-by-13/index.js
@@ -1,8 +1,10 @@
 function thirt(n) {
+  // Declaring the array with the given numbers to be multiplied with
   const nums = [1, 10, 9, 12, 3, 4];
-  let sum = ("" + n)
-    .split("")
-    .reverse()
+
+  let sum = ("" + n) // Converting it into string
+    .split("") // Spliting, it becomes ['1', '10' ..]
+    .reverse() // Reversing the whole array
     .reduce((sum, v, i) => sum + v * nums[i % nums.length], 0);
   return sum === n ? n : thirt(sum);
 }
