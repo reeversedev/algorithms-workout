fd1834dcaeed391e841528fdd2e0c9b9146280b5
Prateek Gogia <prateek_gogia@gridle.io>
2018-11-14 10:13:51 +0530

    [ADDED]: Adding final solution to the problem

--- a-rule-of-divisibility-by-13/index.js
+++ a-rule-of-divisibility-by-13/index.js
@@ -1,19 +1,8 @@
 function thirt(n) {
-  let arrayReverse = n
-    .toString()
+  const nums = [1, 10, 9, 12, 3, 4];
+  let sum = ("" + n)
     .split("")
-    .reverse();
-  const arrayRemainders = [1, 10, 9, 12, 3, 4];
-  const arrayTimes = [];
-
-  for (let i = 0; i < arrayReverse.length; i++) {
-    let sequence = 0;
-
-    if (i % 6 === 0) {
-      sequence = 0;
-    } else {
-      sequence = i % 6;
-    }
-    console.log(sequence);
-  }
+    .reverse()
+    .reduce((sum, v, i) => sum + v * nums[i % nums.length], 0);
+  return sum === n ? n : thirt(sum);
 }
