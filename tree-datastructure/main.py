ed76e82d7d848f66c85e81a7c51430614644f49a
Prateek Gogia <gogia.prateek@hotmail.com>
2019-03-16 16:40:11 +0530

    Implemented search functionality

--- tree-datastructure/main.py
+++ tree-datastructure/main.py
@@ -23,11 +23,11 @@ class binary_search_tree:
                 self._insert(value, curr_node.left_child)
         elif value > curr_node.value:
             if curr_node.right_child == None:
-                curr_node.right_child == node(value)
+                curr_node.right_child = node(value)
             else:
                 self._insert(value, curr_node.right_child)
         else:
-            print "Value already in tree!"
+            print("Value already in tree!")
 
     def print_tree(self):
         if self.root != None:
@@ -36,8 +36,36 @@ class binary_search_tree:
     def _print_tree(self, curr_node):
         if curr_node != None:
             self._print_tree(curr_node.left_child)
-            print str(curr_node.value)
+            print(str(curr_node.value))
             self._print_tree(curr_node.right_child)
+
+    def height(self):
+        if self.root != None:
+            return self._height(self.root, 0)
+        else:
+            return 0
+
+    def _height(self, curr_node, curr_height):
+        if curr_node == None:
+            return curr_height
+        left_height = self._height(curr_node.left_child, curr_height+1)
+        right_height = self._height(curr_node.right_child, curr_height+1)
+        return max(left_height, right_height)
+
+    def search(self, value):
+        if self.root != None:
+            return self._search(value, self.root)
+        else:
+            return False
+
+    def _search(self, value, curr_node):
+        if value == curr_node.value:
+            return True
+        elif value < curr_node.value and curr_node.left_child != None:
+            return self._search(value, curr_node.left_child)
+        elif value > curr_node.value and curr_node.right_child != None:
+            return self._search(value, curr_node.right_child)
+        return False
 
 
 def fill_tree(tree, num_elems=100, max_int=1000):
@@ -49,6 +77,21 @@ def fill_tree(tree, num_elems=100, max_int=1000):
 
 
 tree = binary_search_tree()
-tree = fill_tree(tree)
+# tree = fill_tree(tree)
+
+tree.insert(5)
+tree.insert(1)
+tree.insert(3)
+tree.insert(2)
+tree.insert(7)
+tree.insert(10)
+tree.insert(0)
+tree.insert(20)
+
 
 tree.print_tree()
+
+print("tree height: " + str(tree.height()))
+
+print(tree.search(5))
+print(tree.search(0))
