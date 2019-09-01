93b265847d72e14cabd30f81f4b4997b495e8556
Prateek Gogia <gogia.prateek@hotmail.com>
2019-09-01 13:43:57 +0530

    Added .map() polyfill

--- polyfills/map.js
+++ polyfills/map.js
@@ -1,1 +1,36 @@
-Array.prototype.
+Array.prototype.mapAlbums = function(callback) {
+  var arr = [];
+  for (var i = 0; i < this.length; i++) {
+    arr.push(callback(this[i], i, this));
+  }
+  return arr;
+};
+
+var logicAlbums = [
+  'Bobby Tarantino',
+  'The Incredible True Story',
+  'Supermarket',
+  'Under Pressure'
+];
+
+const mapThoseAlbums = logicAlbums.mapAlbums(function(album) {
+  return album;
+});
+
+console.log(mapThoseAlbums);
+// [
+//   'Bobby Tarantino',
+//   'The Incredible True Story',
+//   'Supermarket',
+//   'Under Pressure'
+// ];
+
+var logicAlbumRatings = [5, 4.5, 4.9, 5];
+
+const squareThoseAlbumRatings = logicAlbumRatings.mapAlbums(function(
+  albumRating
+) {
+  return Math.pow(albumRating, 2);
+});
+
+console.log(squareThoseAlbumRatings);
